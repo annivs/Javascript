35e452bce10cf46e6efe7b91c92adb9152337878
annivs <anni.saarelma@icloud.com>
2024-04-05 13:16:56 +0300

    tehtävä 3

--- Moduuli 1/teht.3.js
+++ Moduuli 1/teht.3.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const num1 = parseInt(prompt("Anna eka numero:"));
-const num2 = parseInt(prompt("EAnna toka numero:"));
-const num3 = parseInt(prompt("EAnna kolmas numero:"));
+const num2 = parseInt(prompt("Anna toka numero:"));
+const num3 = parseInt(prompt("Anna kolmas numero:"));
 
 // Laske summa, tulo ja keskiarvo
 const sum = num1 + num2 + num3;
